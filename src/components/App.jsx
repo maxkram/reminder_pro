b31f3f0778050e3f03b49c8dc7350b8ebacdd261
maxkram <max.kramer@live.com>
2017-07-23 20:00:16 +0300

    cookies added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import '../App.css';
 import { connect } from 'react-redux';
-import { addReminder } from '../actions';
-
+import { addReminder, deleteReminder } from '../actions';
+import moment from 'moment';
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state={
-            text: ''
+            text: '',
+            dueDate: ''
         }
     }
 
     addReminder(){
-        this.props.addReminder(this.state.text);
+        console.log('this.state.dueDate', this.state.dueDate);
+        this.props.addReminder(this.state.text, this.state.dueDate);
+    }
+
+    deleteReminder(id){
+        this.props.deleteReminder(id);
     }
 
     renderReminder(){
@@ -24,7 +30,16 @@ class App extends Component {
                     reminders.map(reminder => {
                         return (
                             <li key={reminder.id} className="list-group-item">
-                                <div>{reminder.text}</div>
+                                <div className="list-item">
+                                    <div>{reminder.text}</div>
+                                    <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                                </div>                                
+                                <div
+                                    className="list-item delete-button"
+                                    onClick={() => this.deleteReminder(reminder.id)}
+                                >
+                                    &#x2715;
+                                </div>
                             </li>
                         )
                     })
@@ -46,6 +61,11 @@ class App extends Component {
                         placeholder='I have to...'
                         onChange={event =>  this.setState({text: event.target.value})}
                         />
+                    <input
+                    className="form-control"
+                    type="datetime-local"
+                    onChange={event => this.setState({dueDate: event.target.value})}
+                    />
                     </div>
                     
                     <button
@@ -70,4 +90,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, { addReminder })(App);
+export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
